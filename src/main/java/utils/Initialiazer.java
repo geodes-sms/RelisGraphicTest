f111b86d658979aa119ab6d67671350781f7ff6a
Mahamat Youssouf Issa <issamahamatyoussouf@gmail.com>
2021-11-17 12:48:14 -0500

    Project updated files

--- src/main/java/utils/Initialiazer.java
+++ src/main/java/utils/Initialiazer.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Initialiazer {
@@ -17,8 +18,9 @@ public class Initialiazer {
     public void init(){
         // setup for using firefox as web navigator
         WebDriverManager.chromedriver().setup();
-
-         webDriver = new ChromeDriver();
+        ChromeOptions ChromeOptions = new ChromeOptions();
+        ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
+         webDriver = new ChromeDriver(ChromeOptions);
         // go to the website
         webDriver.get(RELIS_URL);
         webDriver.manage().window().maximize();
