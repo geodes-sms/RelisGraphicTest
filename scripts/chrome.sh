61d9f7397d262897f51de64c7cafc36a738cd74f
Mahamat Youssouf Issa <47427723+youssouf1@users.noreply.github.com>
2021-11-27 01:35:25 -0500

    Update chrome.sh

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -6,7 +6,12 @@ ls
 cd relisApp
 git checkout develop
 pwd
+sudo rm -r workspace
 
+#copy the workspace 
+cp -R ../workspace .
+echo Copied @@@@@@@@@@@@@@@@@@
+ls
 
 #build ReLis
 #sudo docker-compose build
