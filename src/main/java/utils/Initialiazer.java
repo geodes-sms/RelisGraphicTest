dd5fcb79bbad3acb6384dcd2858733e598be8ad9
Mahamat Youssouf Issa <issamahamatyoussouf@gmail.com>
2021-11-29 06:41:38 +0000

    mise a jour code

--- src/main/java/utils/Initialiazer.java
+++ src/main/java/utils/Initialiazer.java
@@ -1,6 +1,8 @@
 package utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -35,6 +37,8 @@ public class Initialiazer {
         chromeDriver();
         // go to the website
         webDriver.get(LOCAL_URL);
+        System.out.println(webDriver.findElement(By.tagName("body")).getText());
+        for(int i=0; i<5; i++) System.out.println("\n\n");
       // webDriver.manage().window().maximize();
 
 
