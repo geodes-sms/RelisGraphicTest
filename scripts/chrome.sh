a96c0d211fb579f21064e0e315f9d96f2d7d4b47
Mahamat Youssouf Issa <issamahamatyoussouf@gmail.com>
2021-11-29 07:27:16 +0000

    script updated

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -4,19 +4,22 @@
 git clone https://github.com/geodes-sms/relis.git relisApp
 
 cd relisApp
-ls
+
 git checkout develop
 pwd
-cd .. && sudo chmod 777 -R relisApp
 cd relis_app
 mkdir -p logs
 cd ..
+ls
+ 
 #copy the workspace 
 cp -R ../workspace workspace
-cd relisApp
+echo last_pwd
+pwd
 echo changing permission of 'workspace' to 777
 sudo chmod 777 -R workspace
 
+cd .. && sudo chmod 777 relisApp && cd relisApp
 cd relis_deployment
 #build ReLis
 sudo docker-compose build
