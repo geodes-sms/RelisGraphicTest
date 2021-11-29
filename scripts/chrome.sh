82fe72b4e3733ee4f9be1393d876ea33c4084cbd
Mahamat Youssouf Issa <47427723+youssouf1@users.noreply.github.com>
2021-11-28 21:48:49 -0500

    Update chrome.sh

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -2,16 +2,18 @@
 
 
 git clone https://github.com/geodes-sms/relis.git relisApp
-sudo chmod 777 -R relisApp
+
 cd relisApp
 ls
 git checkout develop
 pwd
+cd .. && sudo chmod 777 -R relisApp
 cd relis_app
 mkdir -p logs
 cd ..
 #copy the workspace 
 cp -R ../workspace workspace
+cd relisApp
 echo changing permission of 'workspace' to 777
 sudo chmod 777 -R workspace
 
