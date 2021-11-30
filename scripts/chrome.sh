01d7b2e8bab8afafcbc688e7094350fa142e49fd
Mahamat Youssouf Issa <issamahamatyoussouf@gmail.com>
2021-11-29 20:00:49 -0500

    Project updated files

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -16,10 +16,11 @@ ls
 sudo cp -r ../workspace workspace
 echo last_pwd
 pwd
-#echo changing permission of 'workspace' to 777
-#sudo chmod 777 -R workspace
-#echo chmod 777 to relisApp
+
 cd .. && sudo chmod a+rwx relisApp -R && cd relisApp && ls -l
+#echo changing permission of 'workspace' to 777
+sudo chmod 777 -R workspace
+echo chmod 777 to relisApp
 cd relis_deployment
 #build ReLis
 sudo docker-compose build
